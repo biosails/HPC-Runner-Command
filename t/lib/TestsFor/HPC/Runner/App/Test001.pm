c9abf113a94103f65f5a0c7f72433d16502f45be
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-08-17 13:34:51 +0400

    commiting

--- t/lib/TestsFor/HPC/Runner/App/Test001.pm
+++ t/lib/TestsFor/HPC/Runner/App/Test001.pm
@@ -5,11 +5,8 @@ use HPC::Runner::Command;
 use Cwd;
 use FindBin qw($Bin);
 use File::Path qw(make_path remove_tree);
-use IPC::Cmd qw[can_run];
 use Data::Dumper;
 use Capture::Tiny ':all';
-use Slurp;
-use File::Slurp;
 
 sub test_001 : Tags(prep) {
     my $test = shift;
